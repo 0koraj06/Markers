c3be7ae169139a713c79770b42c6a706137aa277
Jason Koranteng <Jason Koranteng>
2017-04-27 10:40:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a0koraj06/markers/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.a0koraj06.markers;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import java.util.ArrayList;
+
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+
+public class MainActivity extends Activity {
+
+    MapView mv;
+    ItemizedIconOverlay<OverlayItem> items;
+    ItemizedIconOverlay.OnItemGestureListener<OverlayItem> markerGestureListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+        mv = (MapView) findViewById(R.id.map1);
+
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+
+
+        markerGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>()
+        {
+            public boolean onItemLongPress(int i, OverlayItem item)
+            {
+                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            public boolean onItemSingleTapUp(int i, OverlayItem item)
+            {
+                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        };
+    // Marker Icon
+        items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
+       // Individual Overlay Marker Details, Need a title, short description and given location
+        OverlayItem fernhurst = new OverlayItem("Fernhurst", "Village in West Sussex", new GeoPoint(51.05, -0.72));
+        OverlayItem blackdown = new OverlayItem("Blackdown", "highest point in West Sussex", new GeoPoint(51.0581, -0.6897));
+        // Adding Overlay item to the overlay
+        items.addItem(fernhurst);
+        items.addItem(blackdown);
+
+        // Adding the overlay to the list of overlays on the Mapview
+        mv.getOverlays().add(items);
+
+
+    }
+}
